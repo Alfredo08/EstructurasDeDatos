e0e96a7cf795ccdcaebea030301c6f903c35ee41
Alfredo08 <alfredosv08@gmail.com>
2022-02-21 10:14:47 -0600

    Agregando algoritmo remueveDuplicados

--- EstructurasDeDatos/src/com/codingdojo/aplicacion/Aplicacion.java
+++ EstructurasDeDatos/src/com/codingdojo/aplicacion/Aplicacion.java
@@ -1,20 +1,56 @@
 package com.codingdojo.aplicacion;
 
 import com.codingdojo.estrucutras.*;
+import java.util.HashMap;
 
 public class Aplicacion {
+	
+	public static void removerDuplicadosEnDLL( DLL lista ) {
+		HashMap<Integer, Integer> mapa = new HashMap<Integer, Integer>();
+		
+		NodoDLL aux = lista.getHead();
+		NodoDLL prevAux = aux;
+		
+		mapa.put( lista.getHead().getValor(), 1 );
+		while( aux.getNext() != null ) {
+			prevAux = aux;
+			aux = aux.getNext();
+			
+			if( mapa.get(aux.getValor()) == null ) {
+				mapa.put( aux.getValor(), 1 );
+			}
+			else {
+				prevAux.setNext( aux.getNext() );
+				aux.setNext( null );
+				aux = prevAux;
+			}
+		}
+		
+	}
+	
 	public static void main( String args[] ) {
 		DLL listaNumeros = new DLL();
 		
-		NodoDLL nodo1 = new NodoDLL( 10 );
-		NodoDLL nodo2 = new NodoDLL( 20 );
-		NodoDLL nodo3 = new NodoDLL( 15 );
+		NodoDLL nodo1 = new NodoDLL( 1 );
+		NodoDLL nodo2 = new NodoDLL( 3 );
+		NodoDLL nodo3 = new NodoDLL( 1 );
+		NodoDLL nodo4 = new NodoDLL( 4 );
+		NodoDLL nodo5 = new NodoDLL( 1 );
 		
-		listaNumeros.agregaEnPosicion( nodo1, 0 );
-		listaNumeros.agregaEnPosicion( nodo2, 1 );
-		listaNumeros.agregaEnPosicion( nodo3, 1 );
+		
+		listaNumeros.agregaAlFinal( nodo1 );
+		listaNumeros.agregaAlFinal( nodo2 );
+		listaNumeros.agregaAlFinal( nodo3 );
+		listaNumeros.agregaAlFinal( nodo4 );
+		listaNumeros.agregaAlFinal( nodo5 );
+		
+		listaNumeros.imprimePrincipioFin();
+		removerDuplicadosEnDLL( listaNumeros );
+		System.out.print( "\n" );
+		listaNumeros.imprimePrincipioFin();
+		
 
-		
+		/*
 		listaNumeros.imprimePrincipioFin();
 		System.out.print( "\n" );
 		listaNumeros.imprimeFinPrincipio();
@@ -48,5 +84,6 @@ public class Aplicacion {
 		else {
 			System.out.println( "El nodo si se encuentra en el arbol: " + resultado.getValor() );
 		}
+		*/
 	}
 }
